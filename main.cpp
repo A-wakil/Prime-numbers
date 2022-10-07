4660ab4f592d0029fd37ce98c9194d63fd6dd24d
Abdulwakil <105968302+A-wakil@users.noreply.github.com>
2022-10-07 14:49:43 +0000

    User Experience update

--- main.cpp
+++ main.cpp
@@ -7,17 +7,17 @@ int main() {
   cin>>low;
   cout<<"Enter a higher limit: ";
   cin>>high;
-  cout<<"The primenumbers between "<<low<<" and "<<high<<" are\n";
+  cout<<"The prime numbers between "<<low<<" and "<<high<<":\n";
   for (int i=low+1;i<high;i++){
     if (i==2)
-      cout<<2;
+      cout<<2<< " ";
     else
       for (int j=2;j<i;j++){
         if (i%j==0)
           break;
         else
           if (j+1==i)
-            cout<<", "<<i;
+            cout<<i<<" ";
       }
   }
 }
