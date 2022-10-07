a1e6d5562207dc8e95c8d1c4e7ac1df956ece608
Abdulwakil <105968302+A-wakil@users.noreply.github.com>
2022-10-07 14:39:27 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Prime-numbers/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int low;
+  int high;
+  cout<<"Enter a lower limit: ";
+  cin>>low;
+  cout<<"Enter a higher limit: ";
+  cin>>high;
+  cout<<"The primenumbers between "<<low<<" and "<<high<<" are\n";
+  for (int i=low+1;i<high;i++){
+    if (i==2)
+      cout<<2;
+    else
+      for (int j=2;j<i;j++){
+        if (i%j==0)
+          break;
+        else
+          if (j+1==i)
+            cout<<", "<<i;
+      }
+  }
+}
